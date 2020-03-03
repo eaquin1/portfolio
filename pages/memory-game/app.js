6db18b167c53181572cd281e200d556863237d51
Emily Aquin <eaquin1@gmail.com>
2020-03-02 18:35:58 -0600

    refactored memory game

--- pages/memory-game/app.js
+++ pages/memory-game/app.js
@@ -1,159 +1,95 @@
-/*
- * Create a list that holds all of your cards
- */
-const listOfCards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
+const reset = document.getElementById('reset');
+const clicks = document.getElementById('clicks');
+const bestScore = document.getElementById('score');
+let currentOpenCards = [];
+let cardsFlipped = 0;
+let lockBoard = false;
+let lowScore = localStorage.getItem("low-score");
 
-
-console.log(listOfCards);
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+document.addEventListener("DOMContentLoaded", function() {   
+    shuffle();
+    if (lowScore) {
+        bestScore.innerText = lowScore;
+    } else {
+        bestScore.innerText = 0;
     }
+    for (let card of cards) {
+        card.addEventListener('click', handleClick)
+    };    
+}); 
 
-    return array;
+function handleClick(e) {
+    //turn off clicks on flipped cards
+    if (!e.target.classList.contains('closed')) return;
+    if (lockBoard) return;
+    
+    cardIsClicked(e);
+    
+    //if the data attributes of the card match, add the 'match' class and take away the event listener
+    if(currentOpenCards.length === 2 && currentOpenCards[0].attributes["data-cat"].value === currentOpenCards[1].attributes["data-cat"].value){
+        cardsMatch(currentOpenCards);
+    } else if (currentOpenCards.length === 2) {
+       cardsDoNotMatch(currentOpenCards)
+    };
+
+    if (cardsFlipped === 16) {
+        endGame();
+    };
 }
 
-
-function init() {
-
-const shuffling = (cards) => {
-  let shuffleHTML = Array.from(cards).map((i) => {
-    return i.querySelector('i').className;
-  });
-
-  //Shuffle cards
-  shuffle(shuffleHTML);
-
-  //Change class names to shuffled values
-  listOfCards.forEach((item, idx) => {
-    item.querySelector('i').className = shuffleHTML[idx];
-  });
-
-}
-
-//Remove other classes from the cards
-
-const clearDeck = () => {
-  listOfCards.forEach((item) => {
-    item.classList.remove('open', 'show', 'match');
-  });
-}
-clearDeck();
-shuffling(listOfCards);
+function shuffle() {
+    //Shuffle the cards, using the order property from Flexbox
+    cards.forEach(card => {
+        let randomPosition = Math.floor(Math.random() * 17);
+        card.style.order = randomPosition;
+    })
 };
 
-init();
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-//console.log(listOfCards.classList.contains('li.card'));
+function cardIsClicked(e) {
+    //when card is clicked, toggle off 'closed' and toggle on 'open'
+    e.target.classList.toggle('closed');
+    e.target.classList.toggle('open');
 
-listOfCards.forEach((item) => {
-  item.addEventListener('click', () => {
-    displaySymbol(item);
-  })
+    //increase the score 
+    clicks.innerText = parseInt(clicks.innerText) + 1;
+
+    //add selected card to the array to compare to the next click
+    currentOpenCards.push(e.target);
+}
+
+function cardsMatch(arr) {
+    cardsFlipped += 2;
+    for(let matched of arr) {
+        matched.classList.add('match');
+        matched.removeEventListener('click', handleClick);
+    }  
+    currentOpenCards = [];
+} 
+    
+
+function cardsDoNotMatch(arr) {
+    lockBoard = true;
+    //if the cards do no match, flip them back over after one second
+    setTimeout(function() {
+        for(let matched of arr) {
+            matched.classList.toggle('open');
+            matched.classList.toggle('closed');     
+        }
+        lockBoard = false;  
+        currentOpenCards = [];
+    }, 1000)
+}
+
+reset.addEventListener('click', function(e){
+    location.reload();
 });
 
-
- function displaySymbol(item) {
-    item.classList.toggle('show');
-    item.classList.toggle('open');
-    matchingList(item);
- };
-
-  
-let toggledCards = [];
-
- function matchingList(item) {
-  toggledCards.push(item);
-  console.log(toggledCards);
-  if(toggledCards.length == 2) {
-      ifCardsMatch(toggledCards);
-      incrementCounter();
+function endGame() {
+    if ((lowScore > clicks.innerText) || !lowScore) {
+        localStorage.setItem("low-score", clicks.innerText);
+        score.innerText = clicks.innerText;
     }
-  };
-
-
-  
- function ifCardsMatch(listToggled) {
-  if (listToggled[0].firstElementChild.className ===
-     listToggled[1].firstElementChild.className) {
-       console.log("Match");
-       cardsMatch(toggledCards);
-     } else {
-      console.log("do not match");
-      cardsDoNotMatch(toggledCards);
-     }
-  }
- 
-
-function cardsMatch(listToggled) {
-  listToggled.forEach(function(item){
-    item.removeEventListener('click', () => {
-    displaySymbol(item);
-        })
-    item.classList.toggle('match');
-    }) 
-  toggledCards = [];
-};
-
-
-
-function cardsDoNotMatch(listToggled) {
-  setTimeout(() => {
-  listToggled.forEach(function(item){
-    item.classList.toggle('show');
-    item.classList.toggle('open');
-    });
-  toggledCards = [];
-}, 1000);
+    alert('You won!')
 }
 
-let moves = 0;
-function incrementCounter() {
-    const movesCounter = document.querySelector('.moves');
-    moves++;
-    movesCounter.innerHTML = moves;
-    if (moves === 18 || moves === 32) {
-      removeStars();
-    }
-}
-
-function removeStars() {
-  let star = document.querySelector('.stars');
-  console.log(star);
-  star.removeChild(star.firstElementChild);
-}
-
-
-const restart = document.querySelector('.restart');
-
-function restartGame() {
-  //const movesCounter = document.querySelector('.moves');
-  //movesCounter.innerHTML = 0;
-  location.reload();
-  //init();
-}
-restart.addEventListener('click', restartGame);
